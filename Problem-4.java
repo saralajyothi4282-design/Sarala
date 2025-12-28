428401333a70948f5a74db714741cfa21045a0ec
saralajyothi4282-design <saralajyothi4282@gmail.com>
2025-12-28 23:06:08 +0530

    Problem-4.java

--- Problem-4.java
+++ Problem-4.java
@@ -1,26 +1,29 @@
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Problem4
+public class MultipleCounter
   {
-    public static void main(String[] args)
-    {
-        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
-        Map<Integer, Integer> result = new HashMap<>();
 
-        for (int i = 1; i <= 9; i++)
-          {
+    public static Map<Integer, Integer> countMultiples(int[] numbers) 
+    {
+        Map<Integer, Integer> result = new LinkedHashMap<>();
+        for (int i = 1; i <= 9; i++) {
             int count = 0;
-            for (int num : arr)
-              {
-                if (num % i == 0)
-                {
+            for (int num : numbers) {
+                if (num % i == 0) {
                     count++;
                 }
             }
             result.put(i, count);
         }
+        return result;
+    }
 
-        System.out.println(result);
+    public static void main(String[] args)
+    {
+        int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        System.out.println("Input array: ");
+        for (int n : input) System.out.print(n + " ");
+        System.out.println("\nMultiples count: " + countMultiples(input));
     }
 }
