a82f725281444be4e565764fea18936b73c464c9
saralajyothi4282-design <saralajyothi4282@gmail.com>
2025-12-28 23:07:10 +0530

    Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class OddSeries1 {
+public class OddSeries1 
+{
 
-    public static List<Integer> generateOddNumbers(int a) {
+    public static List<Integer> generateOddNumbers(int a)
+    {
         List<Integer> oddNumbers = new ArrayList<>();
-        for (int i = 1; i <= a; i++) {
+        for (int i = 1; i <= a; i++)
+            {
             oddNumbers.add(2 * i - 1);
         }
         return oddNumbers;
