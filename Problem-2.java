6d37ef1d78c69da80f80aab1812678c2cdd531ed
saralajyothi4282-design <saralajyothi4282@gmail.com>
2025-12-28 23:03:04 +0530

    Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,23 +1,18 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Problem2 
-{
-    public static void main(String[] args) 
-  {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+public class OddSeries1 {
 
-        if (a <= 0) 
-        {
-            System.out.println("Invalid input");
-            return;
+    public static List<Integer> generateOddNumbers(int a) {
+        List<Integer> oddNumbers = new ArrayList<>();
+        for (int i = 1; i <= a; i++) {
+            oddNumbers.add(2 * i - 1);
         }
+        return oddNumbers;
+    }
 
-        for (int i = 1; i <= a; i++)
-          {
-            System.out.print((2 * i - 1) + " ");
-        }
-
-        sc.close();
+    public static void main(String[] args) {
+        int a = 4;
+        System.out.println("Input a = " + a + ", Output: " + generateOddNumbers(a));
     }
 }
