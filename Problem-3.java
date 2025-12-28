ed0936de1cfb3b18967f1f432a2c338f3c538593
saralajyothi4282-design <saralajyothi4282@gmail.com>
2025-12-28 23:08:17 +0530

    Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,19 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class OddSeries2 {
+public class OddSeries2
+    {
 
-    public static List<Integer> generateConditionalOddSeries(int a) {
+    public static List<Integer> generateConditionalOddSeries(int a) 
+        {
         List<Integer> oddNumbers = new ArrayList<>();
         int limit = (a % 2 == 0) ? a - 1 : a;
 
-        for (int i = 1; i <= limit; i += 2) {
+        for (int i = 1; i <= limit; i += 2) 
+        {
             oddNumbers.add(i);
         }
         return oddNumbers;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        {
         int a = 6;
         System.out.println("Input a = " + a + ", Output: " + generateConditionalOddSeries(a));
     }
